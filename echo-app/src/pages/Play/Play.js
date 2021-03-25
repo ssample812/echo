5d3cc3f85749ef0b9f296286a7f70bddae38a2df
Rebecca Parker <rfpparker@gmail.com>
2021-03-25 12:51:20 -0500

    Modify play page to use mp3 fileplayer

--- echo-app/src/pages/Play/Play.js
+++ echo-app/src/pages/Play/Play.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import FilePlayer from './Fileplayer'
 
 function Play() {
     return(
         <>
             <div>
                 <h1>Fancy Play Page</h1>
+                <FilePlayer></FilePlayer>
             </div>
         </>
     );
