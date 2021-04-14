e7384e58e046b629575af17f51d8fabdd22ce214
jman793 <jmarz793@yahoo.com>
2021-04-14 15:45:53 -0500

    Add delete to Create

--- echo-app/src/pages/Create/Create.js
+++ echo-app/src/pages/Create/Create.js
@@ -4,6 +4,25 @@ import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import Play from '../Play/Play'
 
 function Create() {
+
+    const [hidden,setHidden] = useState(true);
+    function deleteOnClick(item_id){
+        const url='https://56rrn4nhgh.execute-api.us-east-1.amazonaws.com/songs/create';
+        const header = {
+            "user_id": "Jonah Marz",
+            "item_id": item_id
+        }
+        fetch(url, {
+            method: 'DELETE',
+            headers: header
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .then(alert("Song Deleted"))
+        .then(setHidden(false));
+    
+    }
+    
     return(
         <>
         <div className="container">
@@ -12,6 +31,7 @@ function Create() {
                 <h1>Create Page</h1>
                     <Router>
                         <Button href='/play'>Play</Button>
+                        <Button onClick = {() => deleteOnClick(props.song.item_id)}>Delete</Button>
                         <Switch>
                             <Route path='/play'>
                                 <Play></Play>
