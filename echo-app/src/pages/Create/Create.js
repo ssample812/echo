37eed82b96776208996727784cfcbc5d7ef9d6f4
Samantha Sample <ssample812@gmail.com>
2021-04-18 18:47:28 -0500

    placeholder add note, rest; working clear button, new title

--- echo-app/src/pages/Create/Create.js
+++ echo-app/src/pages/Create/Create.js
@@ -1,5 +1,5 @@
 import React, {useEffect,useState} from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 import {useParams} from 'react-router-dom'
 import OpenSheetMusicDisplay from '../SheetMusic/OpenSheetMusicDisplay'
 
@@ -7,6 +7,27 @@ function Create() {
 
     const params = useParams()
     const [song,setSong] = useState({})
+    const [noteSelected, setNoteSelected] = useState(0);
+    const [restSelected, setRestSelected] = useState(0);
+    // variables for add note
+    // variables for add rest
+    // variables for title form
+    const [title, setTitle] = useState("")
+
+    function clearOnClick(){
+        const url='https://56rrn4nhgh.execute-api.us-east-1.amazonaws.com/songs/create';
+        const body={"note": {"pitch": "R", "position": 0, "duration": 16}};
+        fetch(url, {
+            method: 'PUT',
+            headers: params,
+            body: JSON.stringify(body)
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data[0])
+            setSong(data[0])
+        })
+    }
 
     function deleteOnClick(){
         const url='https://56rrn4nhgh.execute-api.us-east-1.amazonaws.com/songs/create';
@@ -18,7 +39,7 @@ function Create() {
         .then(data => console.log(data))
         .then(alert("Song Deleted"))    
     }
-        
+
     useEffect(() => {
         const url='https://56rrn4nhgh.execute-api.us-east-1.amazonaws.com/songs/create';
         fetch(url, {
@@ -30,6 +51,56 @@ function Create() {
         .then(console.log('done'))
     },[])
 
+    /** Set length options based on beat number */
+    const lengths = [("whole", 4), ("half", 2), ("quarter", 1), ("8th", 0.5)]
+    let noteType = lengths;
+    let noteOptions = null;
+    let restType = lengths;
+    let restOptions = null;
+
+    const changeNoteOptionHandler = (event) => {
+        setNoteSelected(event.target.value);
+    };
+    const changeRestOptionHandler = (event) => {
+        setRestSelected(event.target.value);
+    };
+
+    if (noteSelected == 2 || noteSelected == 3) {
+        noteType = lengths.slice(1)
+    } else if (noteSelected == 4) {
+        noteType = lengths.slice(2)
+    }
+    if (noteType) {
+        noteOptions = noteType.map((el) => <option key={el}>{el}</option>);
+    }
+
+    if (restSelected == 2 || restSelected == 3) {
+        restType = lengths.slice(1)
+    } else if (restSelected == 4) {
+        restType = lengths.slice(2)
+    }
+    if (restType) {
+        restOptions = restType.map((el) => <option key={el}>{el}</option>);
+    }
+
+    
+    function handleNewTitle(e) {
+        const url='https://56rrn4nhgh.execute-api.us-east-1.amazonaws.com/songs/create';
+        const body={"title": title};
+        fetch(url, {
+            method: 'PUT',
+            headers: params,
+            body: JSON.stringify(body)
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data[0])
+            setSong(data[0])
+        })
+        e.preventDefault()
+    }
+
+
     return(
         <>
         <div className="container">
@@ -37,9 +108,144 @@ function Create() {
                 <div className="card-body">
                     <h1>{song.SongName}</h1>
                     <OpenSheetMusicDisplay music={song.MusicXml} />
+                    <div>
+                    <h5>Add a new note:</h5>
+                    <Form inline>
+                        <Form.Label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">
+                            Measure:
+                        </Form.Label>
+                        <Form.Control
+                            as="select"
+                            className="my-1 mr-sm-2"
+                            id="inlineFormCustomSelectPref"
+                            custom
+                        >
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </Form.Control>
+                        <Form.Label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">
+                            Beat:
+                        </Form.Label>
+                        <Form.Control
+                            as="select"
+                            className="my-1 mr-sm-2"
+                            id="inlineFormCustomSelectPref"
+                            custom
+                            onChange={changeNoteOptionHandler}
+                        >
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </Form.Control>
+                        <Form.Label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">
+                            Note:
+                        </Form.Label>
+                        <Form.Control
+                            as="select"
+                            className="my-1 mr-sm-2"
+                            id="inlineFormCustomSelectPref"
+                            custom
+                        >
+                            <option value='C5'>C5</option>
+                            <option value='B4'>B</option>
+                            <option value='A4'>A</option>
+                            <option value='G4'>G</option>
+                            <option value='F4'>F</option>
+                            <option value='E4'>E</option>
+                            <option value='D4'>D</option>
+                            <option value='C4'>C4</option>
+                        </Form.Control>
+                        <Form.Label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">
+                            Length:
+                        </Form.Label>
+                        <Form.Control
+                            as="select"
+                            className="my-1 mr-sm-2"
+                            id="inlineFormCustomSelectPref"
+                            custom
+                        >
+                            {
+                                noteOptions
+                            }
+                        </Form.Control>
+                        <Button type="submit" className="my-1">
+                            Submit
+                        </Button>
+                    </Form>
+                    <br></br>
+                    <h5>Add a new rest:</h5>
+                    <Form inline>
+                        <Form.Label className="my-1 mr-2">
+                            Measure:
+                        </Form.Label>
+                        <Form.Control
+                            as="select"
+                            className="my-1 mr-sm-2"
+                            custom
+                        >
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </Form.Control>
+                        <Form.Label className="my-1 mr-2">
+                            Beat:
+                        </Form.Label>
+                        <Form.Control
+                            as="select"
+                            className="my-1 mr-sm-2"
+                            custom
+                            onChange={changeRestOptionHandler}
+                        >
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </Form.Control>
+                        <Form.Label className="my-1 mr-2">
+                            Length:
+                        </Form.Label>
+                        <Form.Control
+                            as="select"
+                            className="my-1 mr-sm-2"
+                            id="inlineFormCustomSelectPref"
+                            custom
+                        >
+                            {
+                                restOptions
+                            }
+                        </Form.Control>
+                        <Button type="submit" className="my-1">
+                            Submit
+                        </Button>
+                    </Form>
+                    <br></br>
+                    <h5>Change the title:</h5>
+                    <Form inline onSubmit= {e => handleNewTitle(e)}>
+                        <Form.Label className="my-1 mr-2">
+                            Title:
+                        </Form.Label>
+                        <Form.Control
+                            className="mb-2 mr-sm-2"
+                            placeholder={song.SongName}
+                            onChange={e => setTitle(e.target.value)}
+                        />
+                        <Button type="submit" className="mb-2">
+                            Submit
+                        </Button>
+                    </Form>
+                    </div>
+                    <br></br>
                     <div className="d-flex justify-content-between">
                         <Button href='/play'>Play</Button>
-                        <Button onClick = {() => deleteOnClick(song.ItemID)}>Delete</Button>
+                        <div>
+                            <Button onClick = {() => clearOnClick()}>Clear</Button>
+                            <span>  </span>
+                            <Button onClick = {() => deleteOnClick()}>Delete</Button>
+                        </div>
                     </div>
                 </div>
             </div>
