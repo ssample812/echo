f6ce05423d393b900a9bc2d0246dc45f25566ff4
Paul Gillis <pauldg7@gmail.com>
2021-04-19 15:29:45 -0500

    Added simple welcome page, added / path to redirect to /home

--- echo-app/src/pages/Home/Home.js
+++ echo-app/src/pages/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
           <div className="card border border-dark">
             <div className="card-body">
               <h1 className="font-weight-light">Echo</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Welcome to the echo music creator. Please login or signup to begin.</p>
             </div>
           </div>
         </div>
